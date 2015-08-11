2c2ad68e15637b37e39b995cb9c188ccb37ef3d3
Tom Cabanski <tom@cabanski.com>
2015-08-11 13:12:37 -0500

    Upgrade to latest version of template

--- src/main/resources/assets/master/gulpfile.js
+++ src/main/resources/assets/master/gulpfile.js
@@ -1,40 +1,42 @@
-var args = require('yargs').argv,
-  path = require('path'),
-  flip = require('css-flip'),
-  through = require('through2'),
-  gulp = require('gulp'),
-  $ = require('gulp-load-plugins')(),
-  gulpsync = $.sync(gulp),
-  del = require('del'),
-  PluginError = $.util.PluginError;
+var args        = require('yargs').argv,
+    path        = require('path'),
+    flip        = require('css-flip'),
+    through     = require('through2'),
+    gulp        = require('gulp'),
+    $           = require('gulp-load-plugins')(),
+    gulpsync    = $.sync(gulp),
+    PluginError = $.util.PluginError,
+    del         = require('del');
 
 // production mode (see build task)
 var isProduction = false;
 // styles sourcemaps
-var useSourceMaps = true;
-
-log("isProduction=" + isProduction)
-log("useSourceMaps=" + useSourceMaps)
+var useSourceMaps = false;
 
 // Switch to sass mode. 
 // Example:
 //    gulp --usesass
 var useSass = args.usesass;
 
+// Angular template cache
+// Example:
+//    gulp --usecache
+var useCache = args.usecache;
+
 // ignore everything that begins with underscore
 var hidden_files = '**/_*.*';
-var ignored_files = '!' + hidden_files;
+var ignored_files = '!'+hidden_files;
 
 // MAIN PATHS
 var paths = {
-  app: '../app/',
-  markup: 'jade/',
-  styles: 'less/',
+  app:     '../app/',
+  markup:  'jade/',
+  styles:  'less/',
   scripts: 'js/'
 }
 
 // if sass -> switch to sass folder
-if (useSass) {
+if(useSass) {
   log('Using SASS stylesheets...');
   paths.styles = 'sass/';
 }
@@ -58,32 +60,33 @@ var vendor = {
 
 // SOURCES CONFIG 
 var source = {
-  scripts: [paths.scripts + 'app.init.js',
-    paths.scripts + 'modules/*.js',
-    paths.scripts + 'modules/controllers/*.js',
-    paths.scripts + 'modules/directives/*.js',
-    paths.scripts + 'modules/services/*.js',
-    paths.scripts + 'modules/filters/*.js',
-    paths.scripts + 'custom/**/*.js'
+  scripts: [paths.scripts + 'app.module.js',
+            // template modules
+            paths.scripts + 'modules/**/*.module.js',
+            paths.scripts + 'modules/**/*.js',
+            // custom modules
+            paths.scripts + 'custom/**/*.module.js',
+            paths.scripts + 'custom/**/*.js'
   ],
   templates: {
     index: [paths.markup + 'index.*'],
     views: [paths.markup + '**/*.*', '!' + paths.markup + 'index.*']
   },
   styles: {
-    app: [paths.styles + '*.*'],
-    themes: [paths.styles + 'themes/*'],
-    watch: [paths.styles + '**/*', '!' + paths.styles + 'themes/*']
+    app:    [ paths.styles + '*.*'],
+    themes: [ paths.styles + 'themes/*'],
+    watch:  [ paths.styles + '**/*', '!'+paths.styles+'themes/*']
   }
 };
 
 // BUILD TARGET CONFIG 
 var build = {
   scripts: paths.app + 'js',
-  styles: paths.app + 'css',
+  styles:  paths.app + 'css',
   templates: {
     index: '../',
-    views: paths.app
+    views: paths.app,
+    cache: paths.app + 'js/' + 'templates.js',
   }
 };
 
@@ -115,125 +118,157 @@ var compassOptsThemes = {
   image: 'app/img'
 };
 
+var tplCacheOptions = {
+  root: 'app',
+  filename: 'templates.js',
+  //standalone: true,
+  module: 'app.core',
+  base: function(file) {
+    return file.path.split('jade')[1];
+  }
+};
+
+var injectOptions = {
+  name: 'templates', 
+  transform: function(filepath) {
+    return 'script(src=\'' +
+              filepath.substr(filepath.indexOf('app')) +
+            '\')';
+  }
+}
+
 //---------------
 // TASKS
 //---------------
 
 
 // JS APP
-gulp.task('scripts:app', function () {
-  log('Building scripts..');
-  // Minify and copy all JavaScript (except vendor scripts)
-  return gulp.src(source.scripts)
-    .pipe($.jsvalidate())
-    .on('error', handleError)
-    .pipe($.if(useSourceMaps, $.sourcemaps.init()))
-    .pipe($.concat('app.js'))
-    .pipe($.ngAnnotate())
-    .on('error', handleError)
-    .pipe($.if(isProduction, $.uglify({preserveComments: 'some'})))
-    .on('error', handleError)
-    .pipe($.if(useSourceMaps, $.sourcemaps.write()))
-    .pipe(gulp.dest(build.scripts));
+gulp.task('scripts:app', function() {
+    log('Building scripts..');
+    // Minify and copy all JavaScript (except vendor scripts)
+    return gulp.src(source.scripts)
+        .pipe($.jsvalidate())
+        .on('error', handleError)
+        .pipe( $.if( useSourceMaps, $.sourcemaps.init() ))
+        .pipe($.concat( 'app.js' ))
+        .pipe($.ngAnnotate())
+        .on('error', handleError)
+        .pipe( $.if(isProduction, $.uglify({preserveComments:'some'}) ))
+        .on('error', handleError)
+        .pipe( $.if( useSourceMaps, $.sourcemaps.write() ))
+        .pipe(gulp.dest(build.scripts));
 });
 
 
 // VENDOR BUILD
-gulp.task('vendor', gulpsync.sync(['vendor:base', 'vendor:app']));
+gulp.task('vendor', gulpsync.sync(['vendor:base', 'vendor:app']) );
 
 // Build the base script to start the application from vendor assets
-gulp.task('vendor:base', function () {
-  log('Copying base vendor assets..');
-  return gulp.src(vendor.base.source)
-    .pipe($.expectFile(vendor.base.source))
-    .pipe($.if(isProduction, $.uglify()))
-    .pipe($.concat(vendor.base.name))
-    .pipe(gulp.dest(vendor.base.dest))
-    ;
+gulp.task('vendor:base', function() {
+    log('Copying base vendor assets..');
+    return gulp.src(vendor.base.source)
+        .pipe($.expectFile(vendor.base.source))
+        .pipe($.if( isProduction, $.uglify() ))
+        .pipe($.concat(vendor.base.name))
+        .pipe(gulp.dest(vendor.base.dest))
+        ;
 });
 
 // copy file from bower folder into the app vendor folder
-gulp.task('vendor:app', function () {
+gulp.task('vendor:app', function() {
   log('Copying vendor assets..');
 
   var jsFilter = $.filter('**/*.js');
   var cssFilter = $.filter('**/*.css');
 
   return gulp.src(vendor.app.source, {base: 'bower_components'})
-    .pipe($.expectFile(vendor.app.source))
-    .pipe(jsFilter)
-    .pipe($.if(isProduction, $.uglify(vendorUglifyOpts)))
-    .pipe(jsFilter.restore())
-    .pipe(cssFilter)
-    .pipe($.if(isProduction, $.minifyCss()))
-    .pipe(cssFilter.restore())
-    .pipe(gulp.dest(vendor.app.dest));
+      .pipe($.expectFile(vendor.app.source))
+      .pipe(jsFilter)
+      .pipe($.if( isProduction, $.uglify( vendorUglifyOpts ) ))
+      .pipe(jsFilter.restore())
+      .pipe(cssFilter)
+      .pipe($.if( isProduction, $.minifyCss() ))
+      .pipe(cssFilter.restore())
+      .pipe( gulp.dest(vendor.app.dest) );
 
 });
 
 // APP LESS
-gulp.task('styles:app', function () {
-  log('Building application styles..');
-  return gulp.src(source.styles.app)
-    .pipe($.if(useSourceMaps, $.sourcemaps.init()))
-    .pipe(useSass ? $.compass(compassOpts) : $.less())
-    .on('error', handleError)
-    .pipe($.if(isProduction, $.minifyCss()))
-    .pipe($.if(useSourceMaps, $.sourcemaps.write('./maps')))
-    .pipe(gulp.dest(build.styles));
+gulp.task('styles:app', function() {
+    log('Building application styles..');
+    return gulp.src(source.styles.app)
+        .pipe( $.if( useSourceMaps, $.sourcemaps.init() ))
+        .pipe( useSass ? $.compass(compassOpts) : $.less() )
+        .on('error', handleError)
+        .pipe( $.if( isProduction, $.minifyCss() ))
+        .pipe( $.if( useSourceMaps, $.sourcemaps.write() ))
+        .pipe(gulp.dest(build.styles));
 });
 
 // APP RTL
-gulp.task('styles:app:rtl', function () {
-  log('Building application RTL styles..');
-  return gulp.src(source.styles.app)
-    .pipe($.if(useSourceMaps, $.sourcemaps.init()))
-    .pipe(useSass ? $.compass(compassOpts) : $.less())
-    .on('error', handleError)
-    .pipe(flipcss())
-    .pipe($.if(isProduction, $.minifyCss()))
-    .pipe($.if(useSourceMaps, $.sourcemaps.write('./maps')))
-    .pipe($.rename(function (path) {
-      path.basename += "-rtl";
-      return path;
-    }))
-    .pipe(gulp.dest(build.styles));
+gulp.task('styles:app:rtl', function() {
+    log('Building application RTL styles..');
+    return gulp.src(source.styles.app)
+        .pipe( $.if( useSourceMaps, $.sourcemaps.init() ))
+        .pipe( useSass ? $.compass(compassOpts) : $.less() )
+        .on('error', handleError)
+        .pipe(flipcss())
+        .pipe( $.if( isProduction, $.minifyCss() ))
+        .pipe( $.if( useSourceMaps, $.sourcemaps.write() ))
+        .pipe($.rename(function(path) {
+            path.basename += "-rtl";
+            return path;
+        }))
+        .pipe(gulp.dest(build.styles));
 });
 
 // LESS THEMES
-gulp.task('styles:themes', function () {
-  log('Building application theme styles..');
-  return gulp.src(source.styles.themes)
-    .pipe($.if(useSourceMaps, $.sourcemaps.init()))
-    .pipe(useSass ? $.compass(compassOptsThemes) : $.less())
-    .pipe($.if(isProduction, $.minifyCss()))
-    .pipe($.if(useSourceMaps, $.sourcemaps.write('./maps')))
-    .on('error', handleError)
-    .pipe(gulp.dest(build.styles));
+gulp.task('styles:themes', function() {
+    log('Building application theme styles..');
+    return gulp.src(source.styles.themes)
+        .pipe( useSass ? $.compass(compassOptsThemes) : $.less() )
+        .on('error', handleError)
+        .pipe(gulp.dest(build.styles));
 });
 
 // JADE
-gulp.task('templates:index', function () {
-  log('Building index..');
-  return gulp.src(source.templates.index)
-    .pipe($.changed(build.templates.index, {extension: '.html'}))
-    .pipe($.jade())
-    .on('error', handleError)
-    .pipe($.htmlPrettify(prettifyOpts))
-    .pipe(gulp.dest(build.templates.index))
-    ;
+gulp.task('templates:index', ['templates:views'], function() {
+    log('Building index..');
+
+    var tplscript = gulp.src(build.templates.cache, {read: false});
+    return gulp.src(source.templates.index)
+        .pipe( $.if(useCache, $.inject(tplscript, injectOptions)) ) // inject the templates.js into index
+        .pipe( $.jade() )
+        .on('error', handleError)
+        .pipe($.htmlPrettify( prettifyOpts ))
+        .pipe(gulp.dest(build.templates.index))
+        ;
 });
 
 // JADE
-gulp.task('templates:views', function () {
-  log('Building views..');
-  return gulp.src(source.templates.views)
-    .pipe($.if(!isProduction, $.changed(build.templates.views, {extension: '.html'})))
-    .pipe($.jade())
-    .on('error', handleError)
-    .pipe($.htmlPrettify(prettifyOpts))
-    .pipe(gulp.dest(build.templates.views))
-    ;
+gulp.task('templates:views', function() {
+    log('Building views.. ' + (useCache?'using cache':''));
+    
+    if(useCache) {
+
+      return gulp.src(source.templates.views)
+          .pipe($.jade())
+          .on('error', handleError)
+          .pipe($.angularTemplatecache(tplCacheOptions))
+          .pipe( $.if(isProduction, $.uglify({preserveComments:'some'}) ))
+          .pipe(gulp.dest(build.scripts));
+          ;  
+    }
+    else {
+
+      return gulp.src(source.templates.views)
+          .pipe( $.if( !isProduction, $.changed(build.templates.views, { extension: '.html' }) ))
+          .pipe($.jade())
+          .on('error', handleError)
+          .pipe($.htmlPrettify( prettifyOpts ))
+          .pipe(gulp.dest(build.templates.views))
+          ;
+    }
 });
 
 //---------------
@@ -241,14 +276,14 @@ gulp.task('templates:views', function () {
 //---------------
 
 // Rerun the task when a file changes
-gulp.task('watch', function () {
+gulp.task('watch', function() {
   log('Starting watch and LiveReload..');
 
   $.livereload.listen();
 
-  gulp.watch(source.scripts, ['scripts:app']);
-  gulp.watch(source.styles.watch, ['styles:app', 'styles:app:rtl']);
-  gulp.watch(source.styles.themes, ['styles:themes']);
+  gulp.watch(source.scripts,         ['scripts:app']);
+  gulp.watch(source.styles.watch,    ['styles:app', 'styles:app:rtl']);
+  gulp.watch(source.styles.themes,   ['styles:themes']);
   gulp.watch(source.templates.views, ['templates:views']);
   gulp.watch(source.templates.index, ['templates:index']);
 
@@ -256,21 +291,46 @@ gulp.task('watch', function () {
   var livereloadDelay = 1500;
   // list of source file to watch for live reload
   var watchSource = [].concat(
-    source.scripts,
-    source.styles.watch,
-    source.styles.themes,
-    source.templates.views,
-    source.templates.index
-  );
+      source.scripts,
+      source.styles.watch,
+      source.styles.themes,
+      source.templates.views,
+      source.templates.index
+    );
 
   gulp
     .watch(watchSource)
-    .on('change', function (event) {
-      setTimeout(function () {
-        $.livereload.changed(event.path);
+    .on('change', function(event) {
+      setTimeout(function() {
+        $.livereload.changed( event.path );
       }, livereloadDelay);
     });
 
+});
+
+// lint javascript
+gulp.task('lint', function() {
+    return gulp
+        .src(source.scripts)
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
+});
+
+// Remove all files from the build paths
+gulp.task('clean', function(done) {
+    var delconfig = [].concat(
+                        build.styles,
+                        build.scripts,
+                        build.templates.index + 'index.html',
+                        build.templates.views + 'views',
+                        build.templates.views + 'pages',
+                        vendor.app.dest
+                      );
+
+    log('Cleaning: ' + $.util.colors.blue(delconfig));
+    // force: clean files outside current directory
+    del(delconfig, {force: true}, done);
 });
 
 //---------------
@@ -279,28 +339,26 @@ gulp.task('watch', function () {
 
 // build for production (minify)
 gulp.task('build', gulpsync.sync([
-  'prod',
-  'vendor',
-  'assets'
-]));
+          'prod',
+          'vendor',
+          'assets'
+        ]));
 
-gulp.task('prod', function () {
+gulp.task('prod', function() { 
   log('Starting production build...');
-  isProduction = true;
-  useSourceMaps = false;
+  isProduction = true; 
 });
 
-gulp.task('nosourcemaps', ['nosources', 'default']);
-gulp.task('nosources', function () {
-  useSourceMaps = false;
-});
+// build with sourcemaps (no minify)
+gulp.task('sourcemaps', ['usesources', 'default']);
+gulp.task('usesources', function(){ useSourceMaps = true; });
 
 // default (no minify)
 gulp.task('default', gulpsync.sync([
-  'vendor',
-  'assets',
-  'watch'
-]), function () {
+          'vendor',
+          'assets',
+          'watch'
+        ]), function(){
 
   log('************');
   log('* All Done * You can start editing your code, LiveReload will update your browser after any change..');
@@ -308,20 +366,15 @@ gulp.task('default', gulpsync.sync([
 
 });
 
-gulp.task('assets', gulpsync.sync([
-  'cleanAssets',
-  'scripts:app',
-  'styles:app',
-  'styles:app:rtl',
-  'styles:themes',
-  'templates:index',
-  'templates:views'
-]));
+gulp.task('assets',[
+          'scripts:app',
+          'styles:app',
+          'styles:app:rtl',
+          'styles:themes',
+          'templates:index',
+          'templates:views'
+        ]);
 
-gulp.task('cleanAssets', function () {
-  log('Deleting compiled css, js and html');
-  del([build.styles, paths.app + '/views', paths.app + '/pages'], {force: true});
-});
 
 /////////////////////
 
@@ -334,15 +387,15 @@ function handleError(err) {
 
 // Mini gulp plugin to flip css (rtl)
 function flipcss(opt) {
-
+  
   if (!opt) opt = {};
 
   // creating a stream through which each file will pass
-  var stream = through.obj(function (file, enc, cb) {
-    if (file.isNull()) return cb(null, file);
+  var stream = through.obj(function(file, enc, cb) {
+    if(file.isNull()) return cb(null, file);
 
-    if (file.isStream()) {
-      // Todo: isStream!
+    if(file.isStream()) {
+        // Todo: isStream!
     }
 
     var flippedCss = flip(String(file.contents), opt);
@@ -356,5 +409,5 @@ function flipcss(opt) {
 
 // log to console using 
 function log(msg) {
-  $.util.log($.util.colors.blue(msg));
+  $.util.log( $.util.colors.blue( msg ) );  
 }
--- src/main/resources/assets/master/js/custom/README.txt
+++ src/main/resources/assets/master/js/custom/README.txt
@@ -3,7 +3,5 @@ Another better approach is to create a folder for each model in your application
 
 All files here will be concatenated after all files in the modules/ folder.
 
-Dependencies, see file app.init.js
-Routes, see file modules/config.js
-Constants, see file modules/constant.js
-
+Dependencies, see file app.module.js and modules/core/core.module.js
+Routes, see file under modules/routes/
