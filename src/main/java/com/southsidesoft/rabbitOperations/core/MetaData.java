0d2609f5d623cd1f7a222089fd0b00739c683b81
Tom Cabanski <tom@cabanski.com>
2015-08-20 10:35:24 -0500

    Change docker to use elasticHQ instead of kopf

--- src/main/java/com/southsidesoft/rabbitOperations/core/MetaData.java
+++ src/main/java/com/southsidesoft/rabbitOperations/core/MetaData.java
@@ -2,12 +2,17 @@ package com.southsidesoft.rabbitOperations.core;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import static jersey.repackaged.com.google.common.base.Preconditions.checkArgument;
+import static org.eclipse.jetty.util.StringUtil.isNotBlank;
+
 public class MetaData {
     public MetaData(){
         //serialization
     }
 
     public MetaData(String version){
+        checkArgument(isNotBlank(version), "%s cannot be null, empty or whitespace", version);
+
         this.version = version;
     }
 
